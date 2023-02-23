09828362bcae24c79770a8a905bb1a9eb8cc89fd
Mia Duwi Umiati <118716343+MiaDuwiUmiati@users.noreply.github.com>
2023-02-23 21:15:49 +0700

    Delete SOAL PRIORITAS 1_KELILING LUAS LINGKARAN.dart

--- 04_Fundamental Dart/Praktikum/flutter_section_6/SOAL PRIORITAS 1_KELILING LUAS LINGKARAN.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-void main() {
-  // menghitung keliling dan luas lingkaran
-  double jariJari = 14;
-  double kelilingLingkaran = 2 * 3.14 * jariJari;
-  double luasLingkaran = 3.14 * jariJari * jariJari;
-  
-  print("Lingkaran dengan jari-jari $jariJari memiliki keliling $kelilingLingkaran dan luas $luasLingkaran");
-}
